ff2eac3831378be1b7315249aaa186312a4f0f76
sbloch3 <sbloch3@wisc.edu>
2021-11-21 18:51:47 -0600

    Create variableDrills.py

---
+++ variableDrills.py
@@ -0,0 +1,100 @@
+
+
+''' Integers '''
+#1) Make a variable called var1 that is set to 5. And print the variable.
+varOne = 5
+print (varOne)
+#2) Make a variable called var2 that is set to 10. And print the variable.
+varTwo = 10
+print (varTwo)
+#3) Make a variable called var3 that is set to var2. And print the variable.
+varThree = varTwo
+print (varThree)
+#4) Make a variable called x that is set to 24. And print the variable.
+x = 24
+print (x)
+#5) Make a variable called y that is set to 100. And print the variable.
+y = 100
+print (y)
+
+'''FLOATS'''
+#6) Make a variable called z that is set to 1.1. And print the variable.
+z = 1.1
+print (z)
+#7) Make a variable called a that is set to 2.75. And print the variable.
+a = 2.75
+print(a)
+#8) Make a variable called b that is set to a. And print the variable.
+b = a
+print (b)
+#9) Make a variable called c that is set to 100.1. And print the variable.
+c = 100.1
+print(c)
+#10) Make a variable called d that is set to 12.3. And print the variable.
+d = 12.3
+print(d)
+'''STRINGS'''
+#11) Make a variable called name that is set to your name. And print the variable.
+Sarah = "Sarah"
+print (Sarah)
+#12) Make a variable called e that is set to the first letter of your name. And print the variable.
+s = "Sarah"
+print (s)
+#13) Make a variable called f that is set to the last letter of your name. And print the variable.
+f = "b"
+print (f)
+#14) Make a variable called g that is set to "chicken". And print the variable.
+g = "chicken"
+print (g)
+#15) Make a variable called h that is set to the first 3 letters of you name, using the string indexes. And print the variable.
+h = "sar"
+print (h)
+'''BOOLEANS'''
+#16) Make a variable called done that is set to False. And print the variable.
+done = False
+print (done)
+#17) Make a variable called smart that is set to True. And print the variable.
+smart = True
+print(smart)
+#18) Make a variable called i that is set to done. And print the variable.
+i = done
+print(i)
+#19) Make a variable called j that is set to False. And print the variable.
+j = False
+print (j)
+#20) Make a variable called almostDone that is set to True. And print the variable.
+almostDone = True
+print(almostDone)
+'''LISTS'''
+#21) Make a list called listInt of 5 integer variables.
+#listInt
+aOne = 5
+aTwo = 4
+aThree = 3
+print (aThree)
+aFour = 2
+aFive = 1
+#22) Print the 3rd item in listInt, using indexes.
+
+#23) Make a list called listString of 5 string variables.
+#listString
+stringOne = "Sarah"
+stringTwo = "Bloch"
+stringThree = "J"
+stringFour = "SB"
+print (stringFour)
+stringFive = "SJB"
+#24) Print the 4th item in listString, using indexes.
+
+#25) Make a list called listBoolean of 5 boolean variables. Then print each item in the list, using indexes.
+#listBoolean
+list = ["a", "b", "c"]
+print(list[2])
+listVar = list[1]
+print(listVar)
+listThree ="string"
+print(listThree)
+listFour = ["s", "j", "b"]
+print(listFour [1])
+listFive = "sarah"
+print (listFive)
